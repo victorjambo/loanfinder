a1bcc81860c3927c3dd5ed901e78dca51ac389a5
victorjambo <victorjambo@live.com>
2020-05-25 21:01:15 +0300

    chore: remove unnecessary logs

--- src/utils/AdsV2/AdBanner.js
+++ src/utils/AdsV2/AdBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {PublisherBanner} from 'react-native-admob';
 
-import {logError, ERROR, logInfo, INFO} from '../logger';
+import {logError, ERROR} from '../logger';
 import {ADMOB_PROD_IDS, ADMOB_TEST_IDS} from '../../redux/consts';
 
 let admob = Object.assign({}, ADMOB_PROD_IDS);
@@ -21,7 +21,6 @@ class AdBanner extends React.Component {
         adSize="banner"
         adUnitID={admob.ADMOB_BANNER_ID}
         testDevices={[PublisherBanner.simulatorId]}
-        onAdLoaded={() => logInfo(INFO.AD.LOADED.BANNER)}
         onAdFailedToLoad={error =>
           logError(ERROR.AD.FAILED_TO_LOAD.BANNER, error)
         }
